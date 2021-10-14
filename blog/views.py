4a781d6a9b0488da9aaa38effc9935f8fb98a2f3
Stefan <stefanandjelic8@gmail.com>
2021-10-14 14:49:38 -0500

    detail and list templates

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,10 @@ from .models import Post
 
 
 def post_list(request):
-    posts = Post.publish.all()
+    posts = Post.published.all()
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
-def post_detail(request, day, month, year, post):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
